6cfc02bbfa9df5de65349c5ecc70f653eaa4f686
danie <daniel.freitas.ms1@gmail.com>
2020-08-05 15:04:47 -0300

    melhorias na validacao de cpf

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+//const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL ="http://localhost:8080/validacpf?cpf="
 function validar(){
-
+	debugger
 	var txtCPF = document.getElementById("txtCpf").value;
-
+	console.log("conteudo", txtCPF);
 	fetch(URL+txtCPF)
 	   .then(res=>res.json())
 	   .then(res=>verifica(res))
 }
 
-function verifica(res){
-	if (res.status == "valido"){
-		document.getElementById("msgResult").innerHTML = "<h3 class=\"valido\"> CPF Valido </h3>";
 
+function verifica(res){
+
+
+	if(res.status =="conteudo_vazio"){
+		console.log("conteudo vazio");
+		document.getElementById("msgResult").innerHTML = "<h3 class=\"valido\"> Conteúdo vazio </h3>";
+	} 
+	
+	else if (res.status == "valido"){
+		document.getElementById("msgResult").innerHTML = "<h3 class=\"valido\"> CPF Valido </h3>";
 	}
+
 	else{
 		document.getElementById("msgResult").innerHTML = "<h3 class=\"invalido\"> CPF Invalido </h3>";
 	}
